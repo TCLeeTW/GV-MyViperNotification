687d2caf1f23595b9f6841eb79d78c86b64d6243
TC <tc@geovision.com.tw>
2024-05-09 08:51:24 +0800

    Initial Commit

---
+++ MyViperNotification/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyViperNotification
+//
+//  Created by TC on 2024/5/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
